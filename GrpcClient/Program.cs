38145e9d017f0a32f693a78fec41f8e14110c8b0
Thomaz Henrique <treimberg7@gmail.com>
2024-08-06 21:42:20 -0300

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Net.Client;
+using GrpcProject;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Criando um canal
+var channel = GrpcChannel.ForAddress("https://localhost:7170");
+
+// Criando um cliente
+var client = new Greeter.GreeterClient(channel);
+
+// Buscando a resposta
+var reply = await client.SayHelloAsync(
+    new HelloRequest { Name = "Thomaz" });
+
+Console.WriteLine("Retorno do servidor: " + reply.Message);
+
+Console.ReadKey();
